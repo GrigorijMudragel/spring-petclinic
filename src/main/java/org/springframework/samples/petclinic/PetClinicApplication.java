1dd69d56a746ec322f13edd63edea9906b001c7a
Grigorij_Mudragel <Grigorij_Mudragel@EPBYMINW0609.minsk.epam.com>
2017-04-11 23:10:21 +0300

    DDDD

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -30,7 +30,7 @@ public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
         // simple dss comment;
-        int iu = 3;
+        int iu = 4;
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
